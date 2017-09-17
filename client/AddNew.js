51bad5f948a4983f71eab7a2c60b26f29d984a33
dylanch <hchen004@citymail.cuny.edu>
2017-09-17 19:23:20 -0400

    UI updates for addNew

--- client/AddNew.js
+++ client/AddNew.js
@@ -1,14 +1,21 @@
 'use strict';
 var addNewApp = angular.module('addNewApp',[]);
+var years = [];
+
+for (let i =1899; i<=new Date().getFullYear();i++){
+    years.push(i);
+}
 
 var addNewController = ($scope)=>{
     $scope.title = '';
     $scope.ISBN = '';
     $scope.author ='';
+    $scope.year;
+    $scope.years = years;
     $scope.submit = ()=>{
         if($scope.title.trim()==='')
-            alert('Enter all required fields')
+            alert('Enter all required fields');
     };
 };
 
-addNewApp.controller(['$scope','$http',addNewController]);
+addNewApp.controller('addNewController',['$scope','$http',addNewController]);
